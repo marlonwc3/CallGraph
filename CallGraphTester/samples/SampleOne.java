a3633d7ce084475c3ffec3534e0073ab76d63424
Marlon Reghert <mras@cin.ufpe.br>
2015-06-29 16:44:18 -0300

    Update SampleOne.java

--- CallGraphTester/samples/SampleOne.java
+++ CallGraphTester/samples/SampleOne.java
@@ -8,14 +8,9 @@ public class SampleOne {
 	public static void fooA(int k){
 		fooB(k+1);
 	}	
-	public static void fooB(int k){
-		//fooC(k+1);
-	}
+	public static void fooB(int k){}
 	public static void main(String[] args) {
-		//MeuTeste b = new MeuTeste();
-		//b.fooA(0);
 		SampleOne.fooA(0);
-		//SampleOne.fooB(0);
 	}
 	
 }
